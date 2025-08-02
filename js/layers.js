dbbd033551f7b41008f341ba47f0907db3aaea49
qkmd463 <nikitamanakov112@gmail.com>
2025-08-02 18:03:29 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -25,6 +25,57 @@ addLayer("r", {
         {key: "r", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
+	    upgrades: {
+        11: {
+            title: "11",
+            description: "10x point gain.",
+            cost: new Decimal(1),
+        },
+        12: {
+            title: "12",
+            description: "restart boost point.",
+            cost: new Decimal(4),
+            effect() {
+                return player[this.layer].points.add(1).pow(3)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+        
+        },
+        13: {
+            title: "Self-Mult",
+            description: "Multiplies your point gain according to your points.",
+            cost: new Decimal(4),
+            effect() {
+                if  (hasUpgrade('p', 21)) return player.points.add(1).pow(0.15).add(1).pow(1.01)
+                else return player.points.add(1).pow(0.15) 
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
+        },
+        14: {
+            title: "Reverse Synergy-Mult",
+            description: "Multiplies your prestige point gain according to your points.",
+            cost: new Decimal(8),
+            effect() {
+                return player.points.add(1).pow(0.15).pow(buyableEffect('e', 11))
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }
+        },
+        21: {
+            title: "Self-Mult Boost",
+            description: "RAISES YOUR SELF-MULT TO THE POWER OF 1.2!!!111!!!!1!1!!! <br> !!11!! WOOOOOOOOHHHOOOOOOOO (i have autism).",
+            cost: new Decimal(30),
+        },
+        22:{
+            title: "Exp",
+            description: "RAISES YOUR POINT GAIN TO THE POWER OF 1.2!!!!!11111!!!111!! <br> !1!!1!!11 YAAAAAYAYAYYYAYYYYYYY (i have autism pt.2).",
+            cost: new Decimal(100),
+            unlocked() { let SDRedstone = false
+                if (hasMilestone('e', 1)) SDRedstone = true
+                let gdhhwwciwcdccedjcbedhjeb = SDRedstone
+                return gdhhwwciwcdccedjcbedhjeb
+            }
+        }
+    },
 })
 
     
