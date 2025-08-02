3523aee0a78256bab812d28c3d8080f7305f14f9
qkmd463 <nikitamanakov112@gmail.com>
2025-08-02 17:35:02 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -26,26 +26,26 @@ addLayer("r", {
     ],
     layerShown(){return true},
 	    upgrades: {
-	11: {
-    title: "11",
-    description: "10x point gain.",
-    cost: new Decimal(1),
-	if (hasUpgrade('r', 11)) gain = gain.times(10)	
-	    ,
+        11: {
+            title: "11",
+            description: "10x point gain.",
+            cost: new Decimal(1),
+		if (hasUpgrade('r', 11)) gain = gain.times(10),
+		    12: {
 	12: {
-    title: "12",
-    description: "restart boost point.",
-    cost: new Decimal(4),
-    effect() {
-        return player[this.layer].points.add(1).pow(3)
-    },
-    effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
-		}
+            title: "12",
+            description: "restart boost point.",
+            cost: new Decimal(4),
+            effect() {
+                return player[this.layer].points.add(1).pow(3)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+        
         }
+        },
 
-	    }
-
-    })
+    }
+})
     
 
 
