dcf2b25724344a3ddf0fb75227ce3c6ddeacbabd
qkmd463 <nikitamanakov112@gmail.com>
2025-08-02 23:22:45 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -79,7 +79,7 @@ addLayer("r", {
 
 })
 
-addLayer("r", {
+addLayer("p", {
     name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "p", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
