043e0d9407395570f51e6b9c3bcbda112cc6e1db
qkmd463 <nikitamanakov112@gmail.com>
2025-08-02 11:33:42 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -28,10 +28,10 @@ addLayer("r", {
 })
 upgrades: {
 	11: {
-		  title: "Make this whatever you want!",
+		  title: "11",
     description: "10x point gain.",
     cost: new Decimal(1)
-		if (hasUpgrade('p', 11)) gain = gain.times(10)
+		if (hasUpgrade('r', 11)) gain = gain.times(10)
         }
 
 	
