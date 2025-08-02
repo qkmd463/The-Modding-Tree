52eb49bd7c5a8eb6ea22eb60c1bfc1530bb53057
qkmd463 <nikitamanakov112@gmail.com>
2025-08-02 18:40:18 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -39,9 +39,17 @@ addLayer("r", {
                 return player[this.layer].points.add(1).pow(3)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
-
-            }
+            },
+	13: {
+            title: "13",
+            description: "restart boost restart.",
+            cost: new Decimal(100),
+            effect() {
+                return player[this.layer].[this.layer].add(1).pow(0.25)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
         }
+	    }
 })
 
     
