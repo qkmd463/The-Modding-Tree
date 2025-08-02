66e073beefed34386634e3c521b57697f5068b73
qkmd463 <nikitamanakov112@gmail.com>
2025-08-02 11:36:14 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -27,12 +27,7 @@ addLayer("r", {
     layerShown(){return true}
 })
 upgrades: {
-	11: {
-		  title: "11",
-    description: "10x point gain.",
-    cost: new Decimal(1)
-		if (hasUpgrade('r', 11)) gain = gain.times(10)
-        }
+	
 
 	
 
