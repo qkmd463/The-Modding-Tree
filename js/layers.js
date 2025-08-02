7c818b224fac937e6cecd5d96b01faaa44b143b1
qkmd463 <nikitamanakov112@gmail.com>
2025-08-02 20:32:41 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -17,6 +17,7 @@ addLayer("r", {
         mult = new Decimal(1)
 	if (hasUpgrade('r', 13)) mult = mult.times(upgradeEffect('r', 13))
 	if (hasUpgrade('r', 21)) mult = mult.times(15)
+	if (hasUpgrade('r', 22)) mult = mult.times(upgradeEffect('r', 22))    
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
