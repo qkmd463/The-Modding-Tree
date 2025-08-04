f952906124051a481a56be4765548599394f5598
qkmd463 <nikitamanakov112@gmail.com>
2025-08-04 13:13:50 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -89,7 +89,7 @@ addLayer("r", {
     color: "#208ec9",
     requires: new Decimal("1e140"), // Can be a function that takes requirement increases into account
     resource: "prestige", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    baseResource: "restart", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.04, // Prestige currency exponent
