bfdd020f7dec995185a0263bef15c9015af7833a
qkmd463 <nikitamanakov112@gmail.com>
2025-08-04 21:17:25 +0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -45,7 +45,8 @@ function getPointGen() {
 	if (hasUpgrade('r', 12)) gain = gain.times(upgradeEffect('r', 12))
 	if (hasUpgrade('r', 14)) gain = gain.times(upgradeEffect('r', 14))
 	if (hasUpgrade('p', 11)) gain = gain.times(10000)
-	if (hasUpgrade('p', 14)) gain = gain.times(upgradeEffect('p', 14))	
+	if (hasUpgrade('p', 14)) gain = gain.times(upgradeEffect('p', 14))
+	if (hasUpgrade('c', 11)) mult = mult.times(50)
 	return gain
 }
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
