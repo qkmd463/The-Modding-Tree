fe36c9c1350c94b99fb44afb9e137780e7164c1e
qkmd463 <nikitamanakov112@gmail.com>
2025-08-06 00:02:21 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -25,7 +25,6 @@ addLayer("r", {
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
-	if (hasUpgrade('r', 32)) exp = exp.plus(0.01)
         return new Decimal(1)
     },
     passiveGeneration() { 
