8d61bd49e008cdcce65816239593f0109599f441
qkmd463 <nikitamanakov112@gmail.com>
2025-08-07 21:07:41 +0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -65,7 +65,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("F280000000"))
+	return player.points.gte(new Decimal("ee280000000"))
 }
 
 
