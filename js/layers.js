583e3ca6134f2148ae088c63295dc779b63d43f9
qkmd463 <nikitamanakov112@gmail.com>
2025-08-07 19:42:50 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -272,10 +272,10 @@ addLayer("r", {
             },
 	42: {
             title: "42",
-            description: "coins raises points",
+            description: "points raises restart",
             cost: new Decimal("1e3500"),
             effect() {
-                return player.c.points.add(10).log10().pow(0.0001)
+                return player.c.points.add(10).log10().pow(0.0002)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
             },
