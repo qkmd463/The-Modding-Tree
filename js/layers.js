e593fb365c79e383ca5dc38821c03a272fffcdb5
qkmd463 <nikitamanakov112@gmail.com>
2025-08-22 18:19:55 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -13,6 +13,7 @@ addLayer("r", {
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.08, // Prestige currency exponent
+	softcapStart: function() {return new Decimal ("e1e12")},
     softcapPower: function() {return new Decimal (0.01)},
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
