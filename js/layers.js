d123245ae7136846169de691e2877f9f8af216c8
qkmd463 <nikitamanakov112@gmail.com>
2025-08-24 23:39:24 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -828,7 +828,7 @@ addLayer("r", {
             description: "planet raise points.",
             cost: new Decimal(5),
             effect() {
-                return player.pl.points.add(1).mul(90)
+                return player.pl.points.add(1).mul(1000)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
 		},
@@ -837,7 +837,7 @@ addLayer("r", {
             description: "planet raise restart.",
             cost: new Decimal(6),
             effect() {
-                return player.pl.points.add(1).mul(80)
+                return player.pl.points.add(1).mul(100)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
 		},
@@ -846,7 +846,7 @@ addLayer("r", {
             description: "planet raise prestige.",
             cost: new Decimal(7),
             effect() {
-                return player.pl.points.add(1).mul(70)
+                return player.pl.points.add(1).mul(21)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
 		},
@@ -855,7 +855,7 @@ addLayer("r", {
             description: "planet raise coins.",
             cost: new Decimal(8),
             effect() {
-                return player.pl.points.add(1).mul(60)
+                return player.pl.points.add(1).mul(8)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
 		},
@@ -864,7 +864,7 @@ addLayer("r", {
             description: "planet raise diamonds.",
             cost: new Decimal(9),
             effect() {
-                return player.pl.points.add(50)
+                return player.pl.points.add(4)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
 		},
