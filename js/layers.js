f9d5982c5db23a05eb127ff65b439220ded3c8be
qkmd463 <nikitamanakov112@gmail.com>
2025-08-24 16:55:48 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -50,7 +50,7 @@ addLayer("r", {
     layerShown(){return true},
     doReset(resettingLayer) {
         let keep = [];
-        if (hasMilestone("pl", 0) keep.push("upgrades")
+        if (hasMilestone("pl", 0) && resettingLayer=="p, c, d, pl") keep.push("upgrades")
         if (layers[resettingLayer].row > this.row) layerDataReset(this.layer, keep)
 	},
 	    upgrades: {
