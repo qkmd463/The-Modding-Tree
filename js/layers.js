6a8f9aafbb086e10e961b5cd5ec9e2d533d04a73
qkmd463 <nikitamanakov112@gmail.com>
2025-08-26 13:47:34 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -41,6 +41,7 @@ addLayer("r", {
 	if (hasUpgrade('pl', 24)) mult = mult.times(upgradeEffect('pl', 24))
 	if (hasUpgrade('pl', 34)) mult = mult.pow(upgradeEffect('pl', 34))
 	if (hasUpgrade('pl', 43)) mult = mult.pow(upgradeEffect('pl', 43))
+	if (hasUpgrade('r+', 12)) mult = mult.times("e1e10")
 	if (inChallenge('d', 11)) mult = mult.pow(0.0001)
  	if(mult.gte("e1e12")) mult=mult.div("e1e12").pow(0.1).mul("e1e12")
  	if(mult.gte("e1e15")) mult=mult.div("e1e15").pow(0.01).mul("e1e15")
@@ -219,6 +220,33 @@ addLayer("r+", {
     doReset(resettingLayer) {
 
 	},
+	upgrades: {
+        11: {
+            title: "11",
+            description: "e1e10x point gain.",
+            cost: new Decimal(1),
+        },
+        12: {
+            title: "12",
+            description: "e1e10x restart gain.",
+            cost: new Decimal(10),
+        },
+        13: {
+            title: "13",
+            description: "e1e10x prestige gain.",
+            cost: new Decimal(50),
+        },
+        14: {
+            title: "14",
+            description: "e1e10x coins gain.",
+            cost: new Decimal(100),
+        },
+        21: {
+            title: "21",
+            description: "e1e10x diamonds gain.",
+            cost: new Decimal(250),
+        },
+		}
 	}),
 	addLayer("p", {
     name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
@@ -263,6 +291,7 @@ addLayer("r+", {
 	if (hasUpgrade('pl', 25)) mult = mult.times(upgradeEffect('pl', 25))
 	if (hasUpgrade('pl', 35)) mult = mult.pow(upgradeEffect('pl', 35))
 	if (hasUpgrade('pl', 43)) mult = mult.pow(upgradeEffect('pl', 43))
+	if (hasUpgrade('r+', 13)) mult = mult.times("e1e10")
 	if (inChallenge('d', 11)) mult = mult.pow(0.0001)
  	if(mult.gte("e1e12")) mult=mult.div("e1e12").pow(0.1).mul("e1e12")
  	if(mult.gte("e1e15")) mult=mult.div("e1e15").pow(0.01).mul("e1e15")
@@ -464,6 +493,7 @@ addLayer("r+", {
 	if (hasUpgrade('pl', 31)) mult = mult.times(upgradeEffect('pl', 31))
 	if (hasUpgrade('pl', 41)) mult = mult.pow(upgradeEffect('pl', 41))
 	if (hasUpgrade('pl', 43)) mult = mult.pow(upgradeEffect('pl', 43))
+	if (hasUpgrade('r+', 14)) mult = mult.times("e1e10")
 	if (inChallenge('d', 11)) mult = mult.pow(0.0001)
  	if(mult.gte("e1e12")) mult=mult.div("e1e12").pow(0.1).mul("e1e12")
  	if(mult.gte("e1e15")) mult=mult.div("e1e15").pow(0.01).mul("e1e15")
@@ -665,6 +695,7 @@ addLayer("r+", {
 	if (hasUpgrade('pl', 32)) mult = mult.times(upgradeEffect('pl', 32))
 	if (hasUpgrade('pl', 42)) mult = mult.pow(upgradeEffect('pl', 42))
 	if (hasUpgrade('pl', 43)) mult = mult.pow(upgradeEffect('pl', 43))
+	if (hasUpgrade('r+', 21)) mult = mult.times("e1e10")
  	if(mult.gte("e1e12")) mult=mult.div("e1e12").pow(0.1).mul("e1e12")
  	if(mult.gte("e1e15")) mult=mult.div("e1e15").pow(0.01).mul("e1e15")
  	if(mult.gte("e1e18")) mult=mult.div("e1e18").pow(0.0001).mul("e1e18")
