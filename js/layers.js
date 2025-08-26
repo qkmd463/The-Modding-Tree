608ded8747967254f319e9a33ecf605c2d03f8b2
qkmd463 <nikitamanakov112@gmail.com>
2025-08-26 17:56:44 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -365,7 +365,7 @@ addLayer("rr", {
             effectDescription: "diamonds boost restart+.",
             done() { return player.rr.points.gte(1e17) }
 	            effect() {
-                return player.d.points.add(10).log10().log(10).add(1).mul(10)
+                return player.d.points.add(10).log10().log10().add(1).mul(10)
             },
             effectDisplay() { return format(milestoneEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
         },
