c3fbf13cabf16d8523d4cb904576c3ff37d13fb6
qkmd463 <nikitamanakov112@gmail.com>
2025-08-26 17:12:09 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -566,7 +566,7 @@ addLayer("rr", {
     symbol: "p+", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: true,
+        unlocked: false,
 		points: new Decimal(0),
 	    	restart: new Decimal(0),
     }},
