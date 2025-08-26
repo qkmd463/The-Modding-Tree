cdd76a73130cf5989223f1e14d4f6da2468a186e
qkmd463 <nikitamanakov112@gmail.com>
2025-08-26 17:10:59 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -561,7 +561,42 @@ addLayer("rr", {
             },
 	}
 	}),
+	addLayer("pp", {
+    name: "prestige+", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "p+", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+	    	restart: new Decimal(0),
+    }},
+    color: "#1b77a8",
+    requires: new Decimal("e1e30"), // Can be a function that takes requirement increases into account
+    resource: "prestige", // Name of prestige currency
+    baseResource: "restart", // Name of resource prestige is based on
+    baseAmount() {return player.r.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.16e-30, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
 
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    passiveGeneration() { 
+
+        },    
+    hotkeys: [
+        {key: "P", description: "P: reset for prestige+", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+    doReset(resettingLayer) {
+
+	},
+	}),
 	addLayer("c", {
     name: "coins", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "c", // This appears on the layer's node. Default is the id with the first letter capitalized
