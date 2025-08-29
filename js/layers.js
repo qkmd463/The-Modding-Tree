b6b28f96be046b5d7b62053ad270aa137ab00acb
qkmd463 <nikitamanakov112@gmail.com>
2025-08-29 19:47:29 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -659,7 +659,7 @@ addLayer("rr", {
             description: "prestige+ raise points",
             cost: new Decimal(1e108),
             effect() {
-                return player.pp.points.add(10).tetrate(1.05).pow(1e30)
+                return player.pp.points.add(10).tetrate(1.05).pow(1e21)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
 		},
